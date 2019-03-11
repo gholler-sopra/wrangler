9bdbacbc79bb4150ea886c54ae6b4785d510c4bf
Rishabh Budhouliya <rishabh.budhouliya@rishabh-budhouliya.local>
2019-03-11 17:33:37 +0530

    =jceks file path has been harcoded at mycluster

--- wrangler-service/src/main/java/co/cask/wrangler/service/adls/AzureClientSecretService.java
+++ wrangler-service/src/main/java/co/cask/wrangler/service/adls/AzureClientSecretService.java
@@ -68,7 +68,7 @@ public class AzureClientSecretService {
     /* TODO: jceks file location hardcoded for now, it is to be replaced by some other strategy like either reading from
      * core-site.xml or cdap environment, etc. Till this story is defined, we keep it hard-coded
      */
-    c.set(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH,"jceks://hdfs@reflex-platform-raf003/cdap/ADLSCred.jceks");
+    c.set(CredentialProviderFactory.CREDENTIAL_PROVIDER_PATH,"jceks://hdfs@mycluster/etc/security/jceks/adls.jceks");
     CredentialProvider credentialProvider = CredentialProviderFactory.getProviders(c).get(0);
     for (String key : keys) {
       CredentialProvider.CredentialEntry entry = credentialProvider.getCredentialEntry(key);
