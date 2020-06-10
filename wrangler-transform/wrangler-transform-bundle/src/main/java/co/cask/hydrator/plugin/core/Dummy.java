098c6c210eb26ebdfd470ca38dfef8725ef607e3
pierre-alexandre mahé <pierre-alexandre.mahe@soprasteria.com>
2020-06-10 15:18:17 +0200

    move package

---
+++ wrangler-transform/wrangler-transform-bundle/src/main/java/co/cask/hydrator/plugin/core/Dummy.java
@@ -0,0 +1,11 @@
+package co.cask.hydrator.plugin.core;
+
+/**
+ * Make sure a target directory exists when cdap-maven-plgin runs!
+ */
+public class Dummy {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
